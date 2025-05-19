2dab9f10385ed93b4d987156a39ad7bb05de8887
Amanda Yee <amandakyee@gmail.com>
2025-05-19 16:40:48 -0400

    get spacing for home page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,20 @@
-import Links from "./Links"
+import NavBar from "./NavBar"
 
 export default function Main() {
     return (
-        <div>
-            <nav>
-                <Links />
-            </nav>
+        <div className="flex">
+            <div>
+                <NavBar />
+            </div>
+            
+            {/* flex-1 makes second child grow and fill all remaining horitzontal space */}
+            <div className="flex-1"> 
+                <p className="text-[var(--primary)] font-bold text-5xl text-center py-20">
+                    Full service design and styling studio, <br></br>
+                    based in New York.
+                </p>
 
-            <p>
-                Full service design and styling studio, based in New York.
-            </p>
+            </div>
         </div>
     )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,18 @@
+
+import Links from "./Links"
+import Image from "next/image"
+
+export default function NavBar() {
+    return (   
+        <nav className="mx-10">
+            <Image
+            className="py-10"
+            src="/logo-black-rotated.png"
+            alt="Inside Edit Logo"
+            width={40}
+            height={150}
+            />
+            <Links />
+        </nav>
+    )
+}
