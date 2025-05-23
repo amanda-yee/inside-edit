1fb7c717ba904434bb5391b3021c28e839b3589a
Amanda Yee <amandakyee@gmail.com>
2025-05-23 00:33:05 -0400

    under construction page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
 import Main from "@/components/Main";
+import UnderConstruction from "@/components/UnderConstruction";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Main />
       <Footer />
+      {/* <UnderConstruction /> */}
     </div>
   );
 }
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -10,14 +10,16 @@ type ProjectCardProps = {
 export default function ProjectCard( {title, description, imgSrc}: ProjectCardProps ) {
     return (
         <div className="font-geist w-1/3">
-            <Image 
-                className="rounded-xl"
-                src={imgSrc}
-                alt="Project Image"
-                width={350}
-                height={150}
-            />
-
+            <div className="overflow-hidden">
+                <Image 
+                    className="transform transition-transform duration-300 ease-in-out hover:scale-105 hover:cursor-pointer"
+                    src={imgSrc}
+                    alt="Project Image"
+                    width={350}
+                    height={150}
+                />
+            </div>
+            
             <h3 className="font-bold text-xl py-4">
                 {title}
             </h3>
