337989a747babcc79a0d9fbb54a342a7c9c8aa90
Amanda Yee <amandayee@Amandas-Air.lan>
2025-08-22 22:38:46 -0400

    feedback from client

--- src/app/collaborations/page.tsx
+++ src/app/collaborations/page.tsx
@@ -1,5 +1,6 @@
 import NavBarSide from "@/components/NavBarSide";
 import NavBarTop from "@/components/NavBarTop";
+import Image from "next/image";
 
 export default function CollaborationsPage() {
     return (
@@ -18,19 +19,91 @@ export default function CollaborationsPage() {
                 </h1>
 
                 <h2 className="pb-6">
-                    Subheading Subheading
+                    Your Space, Thoughtfully Reimagined
                 </h2>
 
-                <div className="flex gap-12 font-geist">
-                    <div className="flex-1">
+                <div className="gap-12 font-geist">
+                    <div>
                         <p>
-                            Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui  international first-class nulla ut. Punctual adipisicing, essential lovely queen tempor eiusmod irure. Exclusive izakaya charming Scandinavian impeccable aute quality of life soft power pariatur Melbourne occaecat discerning. Qui wardrobe aliquip, et Porter destination Toto remarkable officia Helsinki excepteur Basset hound. Zürich sleepy perfect consectetur.
+                            We don’t do cookie-cutter. We do custom. Every home has its own rhythm — we tune in, layer in texture, add soul, and make it sing. Whether it’s a sun-drenched loft, a brownstone with bones, or a weekend escape in the woods, our designs are built on real-life needs and unexpected moments of joy.
                             <br></br>
                             <br></br>
-                            Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui  international first-class nulla ut. Punctual adipisicing, essential lovely queen tempor eiusmod irure. Exclusive izakaya charming Scandinavian impeccable aute quality of life soft power pariatur Melbourne occaecat discerning. Qui wardrobe aliquip, et Porter destination Toto remarkable officia Helsinki excepteur Basset hound. Zürich sleepy perfect consectetur.
+                            With a deep love for craftsmanship, color, and clever use of space, we turn the everyday into the extraordinary. We’ve built a great network of partners—proof of our collaborative spirit and our ‘let’s make it happen’ attitude.
                         </p>
                     </div>
+                    <div>
+                        <Image 
+                        src="/collab-logos/arteriors.jpg"
+                        alt="Arteriors Logo"
+                        width={100}
+                        height={100}
+                        />
+
+                        <Image 
+                        src="/collab-logos/blu-dot.jpeg"
+                        alt="Blu Dot Logo"
+                        width={100}
+                        height={100}
+                        />
+
+                        <Image 
+                        src="/collab-logos/design-within-reach.png"
+                        alt="Design Within Reach Logo"
+                        width={100}
+                        height={100}
+                        />
+
+                        <Image 
+                        src="/collab-logos/holly-hunt.png"
+                        alt="Holly Hunt Logo"
+                        width={100}
+                        height={100}
+                        />
+
+                        <Image 
+                        src="/collab-logos/kravet.png"
+                        alt="kravet Logo"
+                        width={100}
+                        height={100}
+                        />
+
+                        <Image 
+                        src="/collab-logos/leftbank-art.jpeg"
+                        alt="LeftBank Art Logo"
+                        width={100}
+                        height={100}
+                        />
+
+                        <Image 
+                        src="/collab-logos/lulu-and-georgia.png"
+                        alt="Lulu and Georgia Logo"
+                        width={100}
+                        height={100}
+                        />
+
+                        <Image 
+                        src="/collab-logos/madegoods.png"
+                        alt="MadeGoods Logo"
+                        width={100}
+                        height={100}
+                        />
+
+                        <Image 
+                        src="/collab-logos/maiden-home.png"
+                        alt="Maiden Home Logo"
+                        width={100}
+                        height={100}
+                        />
+
+                        <Image 
+                        src="/collab-logos/verellen.png"
+                        alt="Verellen Logo"
+                        width={100}
+                        height={100}
+                        />
+                    </div>
                 </div>
+
             </div>
         </div>
     );
