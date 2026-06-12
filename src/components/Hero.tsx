0f9be557d81a224b87cd4f10a6d888b3eb48ee2c
Amanda Yee <amanda.yee@siriusxm.com>
2026-06-12 11:48:09 -0700

    update hero image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,26 +6,25 @@ export default function Hero() {
     return (
     <main className="relative w-screen h-screen">
         <Image
-          className="z-0"
+          className="z-0 object-cover object-[center_20%] md:object-[center_10%]"
           src="/hero-image.jpg"
           alt="Hero Image"
           layout="fill" // fill parent container
-          objectFit="cover" // fills the area and crops if needed
           priority // tells next.js to load image asap
         />
 
         {/* Different displays based on screen size */}
-        <div className="block md:hidden absolute z-10 flex flex-col top-[30%] items-center w-full">
+        <div className="block md:hidden absolute z-10 flex flex-col top-8 items-center w-full">
             <Image
-              src="/logo-white.png"
+              src="/logo-black.png"
               alt="Inside Edit Logo"
               width={250}
               height={50}
               priority
             />
         </div>
-        
-        <div className="hidden md:block absolute z-10 flex flex-col top-[30%] ml-10 gap-6 text-[#000000]">
+
+        <div className="hidden md:flex flex-row absolute z-10 top-8 items-center justify-center w-full gap-8 text-[#000000]">
             <Image
               src="/logo-black.png"
               alt="Inside Edit Logo"
